8b232d94bb544db3f2f918ceaba9977a4647c8ab
texasbobs <rsnyder_co@msn.com>
2016-06-16 15:50:40 -0500

    Create thermosync.groovy

---
+++ thermosync.groovy
@@ -0,0 +1,83 @@
+// Automatically generated. Make future change here.
+
+definition(
+name: "Bobs Thermostat Syncer",
+namespace: "smartthings",
+author: "Tim Slagle Modified by: Duane Helmuth/Bob Snyder",
+description: "Adjust a thermostat based on the setting of another thermostat",
+category: "Green Living",
+iconUrl: "http://icons.iconarchive.com/icons/icons8/windows-8/512/Science-Temperature-icon.png",
+iconX2Url: "http://icons.iconarchive.com/icons/icons8/windows-8/512/Science-Temperature-icon.png"
+)
+
+section
+{
+input "thermostat1", "capability.thermostat", title: "Which Master Thermostat?", multi: false, required: true
+input "thermostat2", "capability.thermostat", title: "Which Slave Thermostat?", multi: false, required: true
+input "tempDiff", "number", title: "Temperature Difference Between Master and Slave?", required: true, defaultValue: 2
+input "sendPushMessage", "bool", title: "Send a push notification?", required: false, defaultValue: true
+input "sendSMS", "phone", title: "Send as SMS?", required: false, defaultValue: false
+
+}
+
+def installed(){
+log.debug "Installed called with ${settings}"
+init()
+}
+
+def updated(){
+log.debug "Updated called with ${settings}"
+unsubscribe()
+init()
+}
+
+def init(){
+runIn(60, "temperatureHandler")
+subscribe(thermostat1, "thermostatSetpoint", temperatureHandler)
+subscribe(thermostat2, "thermostatSetpoint", temperatureHandler)
+}
+
+def temperatureHandler(evt) {
+
+log.debug "Temperature Handler"
+//get the latest temp readings and compare
+def MThermostatTemp = thermostat1.latestValue("thermostatSetpoint")
+def SThermostatTemp = thermostat2.latestValue("thermostatSetpoint")
+
+log.debug "Thermostat(M): ${MThermostatTemp}"
+log.debug "Thermostat(S): ${SThermostatTemp}"
+log.debug "Current Temp Diff: ${tempDiff}"
+
+def difference = (SThermostatTemp - MThermostatTemp)
+
+log.debug "Temp Difference: ${difference}"
+
+//if(difference < 0){
+//	difference *= -1
+//    }
+//log.debug "Temp Difference Now: ${difference}"
+
+
+if( difference != tempDiff ){
+	
+		//set the therm fan state to ON
+        def NewTemp = (MThermostatTemp + tempDiff)
+		def msg = "I changed your ${thermostat2} to by sync'ed with ${thermostat1} with of offset of ${tempDiff} degrees.  Now at ${NewTemp}."
+		thermostat2.setCoolingSetpoint(NewTemp)
+        thermostat2.setHeatingSetpoint(NewTemp)
+        thermostat2.poll()
+		log.debug msg
+		sendMessage(msg)
+	
+} 
+}
+
+private sendMessage(msg){
+if (sendPushMessage == true) {
+sendPush(msg)
+}
+if (sendSMS != null) {
+sendSms(sendSMS, msg) 
+}
+
+}
